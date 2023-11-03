b0733a4d89ef6f087ad917220f19459f595085e3
Asranov Baxtinur <asranov0003@gmail.com>
2023-11-03 21:37:08 +0500

    cubes

---
+++ Cubes.js
@@ -0,0 +1,4 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors).
+// Look at the examples below to get an idea of what the function should do.
+
+const cubes = (a) => a ** 3;
